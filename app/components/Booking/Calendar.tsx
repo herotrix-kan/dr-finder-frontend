63c9eb77a35f79d2537c182b8014edccf26089e6
kan <kan.wang@arq.group>
2020-04-14 01:01:48 +1000

    initiate the calendar

--- app/components/Booking/Calendar.tsx
+++ app/components/Booking/Calendar.tsx
@@ -1,6 +1,10 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route, Link } from 'react-router-dom'
-// import SimpleReactCalendar from 'simple-react-calendar'
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
+
 import List from 'components/List';
 import ListItem from 'components/ListItem';
 import LoadingIndicator from 'components/LoadingIndicator';
@@ -10,13 +14,34 @@ import { Doctor } from '../../containers/Doctors/types';
 
 // export type DoctorListProps = Pick<ContainerState, 'loading' | 'error'> & Pick<UserData, 'repos'>;
 function Calendar(doctorSelected: Doctor) {
+  const [dateSelected, dateSelect] = useState(moment());
+  const onChange = e => dateSelect(e.target.value);
+
+  const [focused, setFocused] = useState(false);
   const availableHoursObject = unescape(doctorSelected.availableHours);
   const bookedHoursObject = unescape(doctorSelected.bookedHours);
+
+  const availableDates = ["2020-04-15", "2020-04-20"];
+  const isBlocked = (day: any) => {
+    return availableDates.some(date => day.isSame(date), 'day')
+  }
   { console.info("availableHoursObject:", availableHoursObject) }
   { console.info("bookedHoursObject:", bookedHoursObject) }
   return (
     <div>
-      {/* <SimpleReactCalendar activeMonth={new Date()} /> */}
+      <SingleDatePicker
+        date={dateSelected} // momentPropTypes.momentObj or null
+        // onDateChange={(date: any) => dateSelect(date)} // PropTypes.func.isRequired
+        numberOfMonths={window.innerWidth < 600 ? 1 : 2}
+        onDateChange={date => onChange({ target: { value: date } })}
+        focused={focused} // PropTypes.bool
+        onFocusChange={({ focused }: any) => setFocused(focused)} // PropTypes.func.isRequired
+        // isDayBlocked={isBlocked}
+        isDayBlocked={m => m.day() === 6 || m.day() === 0}
+        // isOutsideRange={(day: any)=>!isInclusivelyAfterDay(day, moment())}
+        id="1" // PropTypes.string.isRequired,
+      />
+
     </div>
   );
 
