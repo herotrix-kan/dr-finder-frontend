a9c53aba0db43896958e80685a69c078c8a7a833
kan <kan.wang@arq.group>
2020-04-14 23:40:46 +1000

    updated the calendar function

--- app/components/Booking/Calendar.tsx
+++ app/components/Booking/Calendar.tsx
@@ -15,18 +15,30 @@ import { Doctor } from '../../containers/Doctors/types';
 // export type DoctorListProps = Pick<ContainerState, 'loading' | 'error'> & Pick<UserData, 'repos'>;
 function Calendar(doctorSelected: Doctor) {
   const [dateSelected, dateSelect] = useState(moment());
+  const weekdays = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   const onChange = e => dateSelect(e.target.value);
 
   const [focused, setFocused] = useState(false);
-  const availableHoursObject = unescape(doctorSelected.availableHours);
-  const bookedHoursObject = unescape(doctorSelected.bookedHours);
+  const availableHoursObject = JSON.parse(doctorSelected.availableHours.replace(/\"/g, "\""));
+  const bookedHoursObject = JSON.parse(doctorSelected.bookedHours.replace(/\"/g, "\""));
 
-  const availableDates = ["2020-04-15", "2020-04-20"];
-  const isBlocked = (day: any) => {
-    return availableDates.some(date => day.isSame(date), 'day')
+  const getDateSelected = dateSelected.format('YYYY-MM-DD');
+  const getDaySelected = weekdays[dateSelected.isoWeekday()];
+  const getDaySelectedAvailableHours = availableHoursObject[getDaySelected];
+
+  let unvailableDates = [];
+  for (let [key, value] of Object.entries(availableHoursObject)) {
+    console.log(`${key}: ${value}`);
+    if (value.length === 0) {
+      unvailableDates.push(key);
+    }
   }
   { console.info("availableHoursObject:", availableHoursObject) }
+  { console.info("unvailableDates:", unvailableDates) }
   { console.info("bookedHoursObject:", bookedHoursObject) }
+  { console.info("date selected:", getDateSelected) }
+  { console.info("day selected:", getDaySelected) }
+  { console.info("show available time:", getDaySelectedAvailableHours) }
   return (
     <div>
       <SingleDatePicker
@@ -37,11 +49,18 @@ function Calendar(doctorSelected: Doctor) {
         focused={focused} // PropTypes.bool
         onFocusChange={({ focused }: any) => setFocused(focused)} // PropTypes.func.isRequired
         // isDayBlocked={isBlocked}
-        isDayBlocked={m => m.day() === 6 || m.day() === 0}
+        // isDayBlocked={m => m.day() === 6 || m.day() === 0}
+        isDayBlocked={(day: any) => unvailableDates.includes(moment.weekdays(day.weekday()))}
         // isOutsideRange={(day: any)=>!isInclusivelyAfterDay(day, moment())}
         id="1" // PropTypes.string.isRequired,
       />
-
+      <div>
+        <h3>Available hours of the selected date</h3>
+        {getDaySelectedAvailableHours &&
+          getDaySelectedAvailableHours.map((hours, key) => (
+            <p key={`key-${key}`}>{hours}</p>
+          ))}
+      </div>
     </div>
   );
 
